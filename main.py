14e265a60da38f088197268543f42302d8ac8b23
Caio Casimiro <caiorcasimiro@gmail.com>
2021-03-31 08:48:03 -0300

    adds endpoints to be filled with code. adds execution permisison on start.sh. ignoring tmp files

--- main.py
+++ main.py
@@ -6,3 +6,13 @@ app = FastAPI()
 @app.get("/sum")
 async def sum(a: int, b: int):
     return a + b
+
+
+@app.get("/multiply")
+async def multiply(a: int, b: int):
+    return 0  # add code here
+
+
+@app.get("/substract")
+async def substract(a: int, b: int):
+    return 0  # add code here
